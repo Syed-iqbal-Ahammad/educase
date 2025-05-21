6ffce4331e807ad06658122c29f4f7867c9ce9f5
Syed Iqbal <syediqbalahammad333@gmail.com>
2025-05-21 16:42:35 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+const App = () => {
+  return (
+    <div className='bg-gray-900 text-white h-screen flex items-center justify-center'>
+      hai
+    </div>
+  )
+}
+
+export default App
+
